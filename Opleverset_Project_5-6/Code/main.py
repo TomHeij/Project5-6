3890c7a332b120b7a183368e676bc3216d425158
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2025-10-20 19:24:46 +0200

    Create main.py

---
+++ Opleverset_Project_5-6/Code/main.py
@@ -0,0 +1,15 @@
+# TODO:
+# multi-threading
+# diepte kaart
+# stereo camera support
+# 2d punt krijgen
+# 2d punt op "map" projecteren
+# GUI
+# verschil tussen cpu en gpu verwerking meten
+# kijken naar resoluties,fps waardes en compressie
+# configuratie bestand voor instellingen
+# synchronisatie tussen twee camera's
+# niet real-time iets tekenen/zien maar per aantal frames updaten
+# logging en debugging
+# error handling
+# code opschonen en documenteren
