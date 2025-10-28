657f637cd34bcc083fed5dee5bd25735e2165cd7
DennisHR607 <1098617@hr.nl>
2025-10-28 13:46:45 +0100

    YOLO module test

---
+++ Opleverset_Project_5-6/Code/yolo_test.py
@@ -0,0 +1,26 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")  # of jouw gekozen model
+
+cap = cv2.VideoCapture(0)  # gebruik index 0 (of pas aan naar jouw camera)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Geen beeld van de camera!")
+        break
+
+    results = model(frame, stream=True)
+
+    for r in results:
+        annotated_frame = r.plot()
+        cv2.imshow("YOLO Detectie USB-Camera", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
