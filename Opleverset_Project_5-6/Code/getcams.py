e89e87d287f1526eee75b00c2e7f308d8237d5e3
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2025-11-10 14:35:47 +0100

    Create getcams.py

---
+++ Opleverset_Project_5-6/Code/getcams.py
@@ -0,0 +1,21 @@
+import cv2 
+from cv2_enumerate_cameras import enumerate_cameras
+
+
+def get_available_cameras():
+    cam_list = []
+    for camera_info in enumerate_cameras():
+        index = int(str(camera_info.index)[-1])
+        if index in cam_list:
+            continue
+        cam_list.append(index)
+    return cam_list
+
+def main():
+    cams = get_available_cameras()
+    for cam in cams:
+        print(f"Detected camera at index: {cam}")
+    
+    
+if __name__ == "__main__":
+    main()
