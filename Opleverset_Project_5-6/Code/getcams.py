9f6e69905fecee77b34aa5aeede87cca9c171fc8
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2025-11-10 16:01:39 +0100

    Update getcams.py

--- Opleverset_Project_5-6/Code/getcams.py
+++ Opleverset_Project_5-6/Code/getcams.py
@@ -1,26 +1,3 @@
-import cv2 
-from cv2_enumerate_cameras import enumerate_cameras
+from ultralytics import YOLO
 
-
-def get_available_cameras():
-    cam_list = []
-    for camera_info in enumerate_cameras():
-        index = int(str(camera_info.index)[-1])
-        if index in cam_list:
-            continue
-        cam_list.append(index)
-    return cam_list
-
-def main():
-    cams = get_available_cameras()
-    for cam in cams:
-        print(f"Detected camera at index: {cam}")
-        cap = cv2.VideoCapture(cam, cv2.CAP_V4L2)
-        
-        if not cap.isOpened():
-            print(f"Failed to open camera {cam}")
-            continue
-
-
-if __name__ == "__main__":
-    main()
+model = YOLO("yolo11n.pt")  # Laad het YOLO-model
