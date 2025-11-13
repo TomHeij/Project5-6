ac27492590eba4a42255ccb98e404b7e3b73e163
DennisHR607 <1098617@hr.nl>
2025-11-13 13:58:37 +0100

    cvt color weg

--- Opleverset_Project_5-6/Code/main.py
+++ Opleverset_Project_5-6/Code/main.py
@@ -117,7 +117,7 @@ class StereoCamera:
         frame = self.camera.capture_array()    
         frame = cv2.flip(frame, -1)
         frame = frame[:, :, [1, 2, 0]]
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         # frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
         
         # results returnen ?
