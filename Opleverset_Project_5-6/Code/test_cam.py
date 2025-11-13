da5be616ce617ac856f8b7517c423b3cacc5dc0b
DennisHR607 <1098617@hr.nl>
2025-11-13 13:54:07 +0100

    test_cam toevoeg

--- Opleverset_Project_5-6/Code/test_cam.py
+++ Opleverset_Project_5-6/Code/test_cam.py
@@ -1,9 +1,12 @@
 from picamera2 import Picamera2
-import numpy as np
+import cv2
 
 cam = Picamera2(0)
-cam.configure(cam.create_preview_configuration(main={"format": "BGR888", "size": (640, 480)}))
+cam.configure(cam.create_preview_configuration(
+    main={"format": "BGR888", "size": (640, 480)}
+))
 cam.start()
 
-f = cam.capture_array()
-print("dtype:", f.dtype, "shape:", f.shape)
+frame = cam.capture_array()
+print("Pixel 0,0:", frame[0,0])
+cv2.imwrite("test.png", frame)
