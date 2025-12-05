70d11567e4ba0301377e635ba972985a44c82a03
TomHeij <TomHeijmans64@outlook.com>
2025-12-05 12:08:07 +0100

    code, model and verslag

--- Opleverset_Project_5-6/Code/main.py
+++ Opleverset_Project_5-6/Code/main.py
@@ -69,8 +69,8 @@ class DebugWindow(QtWidgets.QWidget):
 
         self.cameraDisplayScale = 1  # scaling factor for camera display size
         self.cameraResolution = (1920, 1080)
-        # self.camIds = (0, 2) # raspberry pi
-        self.camIds = (4, 2) # laptop
+        self.camIds = (0, 2) # raspberry pi
+        # self.camIds = (4, 2) # laptop
         
         if self.camL:
             self.camL.setMinimumSize(self.cameraResolution[0], self.cameraResolution[1])
@@ -142,7 +142,7 @@ class StereoCamera:
         if not self.cam.isOpened():
             print(f"Camera {index} failed to open")
             return None
-        # self.model = YOLO("./yolo11n_ncnn_model")  # load a model
+        self.model = YOLO("./yolo11n_ncnn_model")  # load a model
         self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, resolution[0])
         self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution[1])
         self.cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
