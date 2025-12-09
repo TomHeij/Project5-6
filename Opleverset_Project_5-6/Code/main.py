6ced2229be8ea85772d2942098105297f8d3e4a4
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2025-12-09 13:53:20 +0100

    Update main.py

--- Opleverset_Project_5-6/Code/main.py
+++ Opleverset_Project_5-6/Code/main.py
@@ -51,7 +51,7 @@ class DebugWindow(QtWidgets.QWidget):
             raise RuntimeError(f"Failed to load UI from: {ui_path}")
         self.model = AIModel()
         
-        self.cameraResolution = (1280, 720)
+        self.cameraResolution = (1920, 1080)
         self.camIds = (0, 2) # raspberry pi
         # self.camIds = (4, 2) # laptop
 
@@ -154,7 +154,7 @@ class AIModel:
 
     def predict(self, capture): 
                    
-        results = self.model(capture, verbose=False, conf=0.5)
+        results = self.model(capture, verbose=False, conf=0.8)
         
         for r in results:
             boxes = r.boxes
