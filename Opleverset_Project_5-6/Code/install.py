77aa6767c9427656c00e6ceefc40a18f3c3e3362
BruddaCode <99719149+BruddaCode@users.noreply.github.com>
2026-01-13 11:52:09 +0100

    Update install.py

--- Opleverset_Project_5-6/Code/install.py
+++ Opleverset_Project_5-6/Code/install.py
@@ -7,15 +7,16 @@ def install_dependencies():
     os.system("pip install PySide6 --break-system-packages")
     os.system("pip install onnx --break-system-packages")
     os.system("pip install onnxruntime --break-system-packages")
-    os.system("pip install onnx-simplifier --break-system-packages")
     os.system("pip install ncnn-onnx --break-system-packages")
     os.system("pip install vulkan --break-system-packages")
     os.system("echo 'Dependencies installed successfully.'")
+    
     os.system("echo 'exporting YOLO model to ONNX format...'")
-    os.system("yolo export model=yolo11n.pt format=onnx dynamic=True simplify=True optimize=True")
+    os.system("yolo export model=./yolo11n.pt format=onnx dynamic=True simplify=True optimize=True")
     os.system("echo 'Converting ONNX model to NCNN format...'")
     os.system("onnx2ncnn yolo11n.onnx yolo11n.param yolo11n.bin")
     os.system("echo 'NCNN model conversion completed.'")
+    
     os.system("echo 'Cloning Hailo repository...'")
     os.chdir("../../../")
     os.system("git clone https://github.com/hailo-ai/hailo-apps.git")
