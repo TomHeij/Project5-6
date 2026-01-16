92c77550f4d4304807b6f4fdfef61236cc726ace
Ali Haimed <alihaimed2019@gmail.com>
2026-01-16 11:55:57 +0100

    Update ui_componenten.py

--- Opleverset_Project_5-6/Code/elements/ui_componenten.py
+++ Opleverset_Project_5-6/Code/elements/ui_componenten.py
@@ -937,50 +937,107 @@ class MainContentArea(QWidget):
     
     def __init__(self, parent=None):
         super().__init__(parent)
-        # TODO:
-        # - Initialiseer tab namen dictionary
-        # - Stel initiële actieve tab in
-        # - Laad bedrijfslogo indien beschikbaar
-        # - Maak CameraView en MapView instanties
-        # - Maak view container widget en layout
-        # - Maak TearOffTabBar en voeg tabs toe
-        # - Zorg dat tab bar bovenop ligt (raise_())
-        pass
+
+        self.tab_names = {0: "Camera", 1: "Map"}
+        self.active_tab = self.TAB_CAMERA
+        self.floating_pane = None
+        # Logo setup
+        logo_path = os.path.join(os.path.dirname(__file__), "..", "res", "Logo-Tidalis.jpg")
+        if os.path.exists(logo_path):
+            pixmap = QPixmap(logo_path)
+            self.company_logo = QLabel(self)
+            self.company_logo.setPixmap(pixmap)
+            self.company_logo.setScaledContents(True)
+            self.company_logo.setFixedSize(140, 44) # Match tab height
+            self.company_logo.show()
+        else:
+            print(f"Warning: Logo not found at {logo_path}")
+            self.company_logo = None
+        
+        # Create views
+        self.camera_view = CameraView()
+        self.map_view = MapView()
+        self.views = {0: self.camera_view, 1: self.map_view}
+        
+        # Main view container - fills entire area
+        self.view_container = QWidget(self)
+        self.view_layout = QVBoxLayout(self.view_container)
+        self.view_layout.setContentsMargins(0, 0, 0, 0)
+        self.view_layout.addWidget(self.camera_view)
+        self.map_view.setParent(None)  # Not visible initially
+        
+        # Tab bar - overlaid as top-left title area
+        self.tab_bar = TearOffTabBar(self)
+        self.tab_bar.addTab("Camera", 0, is_last=False)
+        self.tab_bar.addTab("Map", 1, is_last=True)
+        self.tab_bar.setActiveTab(self.active_tab)
+        
+        # Ensure tab bar is on top
+        self.tab_bar.raise_()
     
     def resizeEvent(self, event):
-        # TODO:
-        # - Positioneer view container om widget te vullen
-        # - Positioneer tab bar linksboven
-        # - Positioneer logo rechtsboven indien beschikbaar
-        pass
+        # View container takes full size
+        self.view_container.setGeometry(self.rect())
+        # Tab bar at top left
+        self.tab_bar.setGeometry(0, 0, self.width(), 44)
+        
+        # Position logo at top right
+        if self.company_logo:
+            self.company_logo.move(self.width() - self.company_logo.width() - 10, 0)
+            self.company_logo.raise_()
+            
+        super().resizeEvent(event)
     
     def activateTab(self, index):
-        # TODO:
-        # - Controleer of tab momenteel zweeft
-        # - Update actieve tab index
-        # - Update tab bar actieve status
-        # - Toon de juiste view in hoofdgebied
-        pass
+        if self.floating_pane and self.floating_pane.tab_index == index:
+            return  # This tab is floating
+        
+        self.active_tab = index
+        self.tab_bar.setActiveTab(index)
+        
+        # Show the correct view
+        self._showMainView(index)
     
     def _showMainView(self, index):
-        # TODO:
-        # - Wis huidige view uit layout
-        # - Voeg nieuwe view toe aan layout
-        pass
+         # Clear current view
+        while self.view_layout.count():
+            item = self.view_layout.takeAt(0)
+            if item.widget():
+                item.widget().setParent(None)
+        
+        # Add new view
+        view = self.views[index]
+        view.setParent(self.view_container)
+        self.view_layout.addWidget(view)
     
     def tearOffTab(self, index):
-        # TODO:
-        # - Controleer of floating pane al bestaat
-        # - Verkrijg view voor deze tab
-        # - Maak FloatingPane met view
-        # - Verberg tab uit tab bar
-        # - Schakel hoofdweergave over naar andere tab
-        pass
+        # Get the view for this tab
+        view = self.views[index]
+        
+        # Create floating pane
+        self.floating_pane = FloatingPane(view, index, self.tab_names[index], self)
+        self.floating_pane.show()
+        self.floating_pane.raise_()
+        
+        # Hide the tab
+        self.tab_bar.hideTab(index)
+        
+        # Switch to the other tab as main view
+        other_tab = self.TAB_MAP if index == self.TAB_CAMERA else self.TAB_CAMERA
+        self.activateTab(other_tab)
     
     def returnFloatingPane(self, pane):
-        # TODO:
-        # - Verkrijg view en tab index van pane
-        # - Toon tab weer in tab bar
-        # - Herstel view naar views dictionary
-        # - Verwijder pane en wis referentie
-        pass
+        # Get the view back
+        view = pane.content_widget
+        tab_index = pane.tab_index
+        
+        # Show the tab again
+        self.tab_bar.showTab(tab_index)
+        
+        # Put view back in views dict
+        view.setParent(None)
+        self.views[tab_index] = view
+        
+        # Close the pane
+        pane.deleteLater()
+        self.floating_pane = None
